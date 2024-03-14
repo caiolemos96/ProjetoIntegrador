c42fe1428c7db1a6ca20e9e9e9444d0b5c427e08
Caio Lemos <caio96.lemos@gmail.com>
2024-03-13 21:00:57 -0300

    scripts

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const botaoCadastros = document.getElementById('cadastros')
+const botaoVerProduto = document.getElementById('btn-verProduto')
+const botaoExpedicao = document.getElementById('btn-expedicao')
+const botaoCadastrar = document.getElementById('btn-cadastro-produto')
+
+let nomeProduto = document.getElementById('nome-produto')
+let skuProduto = document.getElementById('sku-produto')
+let custoProduto = document.getElementById('custo-produto')
+let validadeProduto = document.getElementById('validade-produto')
+let qtdEstoque = document.getElementById('qtd-estoque')
+
+console.log(nomeProduto)
